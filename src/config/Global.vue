e37279d1449c03f43cfd7e653179b63d130e69a9
hehuashen <2313480632@qq.com>
2024-02-02 21:43:56 +0800

    First comit

--- src/config/Global.vue
+++ src/config/Global.vue
@@ -1,7 +1,7 @@
 <script>
 
 // const URL = "http://localhost:20000/"; // 生产环境
-const URL = "http://107.174.67.157:20000/"; // 生产环境
+const URL = "http://107.174.67.157:20001/"; // 生产环境
 
 
 export default
